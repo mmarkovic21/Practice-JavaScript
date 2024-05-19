c83f7bfa0e01ea4a0bef43c9a8dcb3e952d4577e
Marija Markovic <551-2022@ftn.edu.rs>
2024-05-19 11:15:16 +0200

    menu and slider

---
+++ Site with some JS functionalities/WeDesign/js/scripts.js
@@ -0,0 +1,63 @@
+// Mobile Menu  //sliding-header-menu-outer
+document.getElementById("hamburguer-icon").onclick = function() {
+  document.getElementById("sliding-header-menu-outer").style.right = "0";
+}
+
+document.getElementById("sliding-header-menu-close-button").onclick = function (){
+  document.getElementById("sliding-header-menu-outer").style.right = "-320px";
+}
+
+
+// About us Tab
+
+var aboutUs = {
+  "Mission": "Duis ac leo nisi. Mauris nec ex id lorem commodo rutrum rutrum a est. Cras facilisis sit amet lectus non posuere. Nullam non magna non enim blandit elementum.",
+  "Vision": "Praesent ut lacinia neque, faucibus suscipit quam. Duis sed nunc rutrum, tempor mi at, euismod nibh.",
+  "Values": "<ul><li>Nunc iaculis</li><li>Donec dictum fringilla</li><li>Duis convallis tortor ultrices</li><li>Curabitur in est lectus</li><li>Maecenas condimentum elit</li></ul>"
+};
+
+var unseletectedColor = "#646872";
+var seletectedColor = "#2A2D34";
+
+let selectedValue = document.getElementsByClassName("single-tab");
+
+for(let i=0; i<selectedValue.length; i++){
+  let clickedItem = this.innerHTML;
+  document.getElementsByClassName("box-text").innerHTML = 
+}
+
+
+
+// Service slider
+
+var ourServices = [
+  {
+    'title': 'Web design',
+    'text': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent finibus tincidunt sem non sodales. Nunc et quam in magna vehicula sollicitudin. Aliquam erat volutpat. Maecenas dolor mi, aliquet ac quam aliquet, condimentum dictum nisi.'
+  },
+
+  {
+    'title': 'Branding',
+    'text': 'Praesent finibus tincidunt sem non sodales. Nunc et quam in magna vehicula sollicitudin. Aliquam erat volutpat. Maecenas dolor mi, aliquet ac quam aliquet, condimentum dictum nisi.'
+  },
+
+  {
+    'title': 'Digital Marketing',
+    'text': 'Nunc et quam in magna vehicula sollicitudin. Aliquam erat volutpat. Maecenas dolor mi, aliquet ac quam aliquet, condimentum dictum nisi. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent finibus.'
+  }
+  
+];
+
+
+
+
+
+// Footer
+
+
+
+  
+   
+
+
+   
